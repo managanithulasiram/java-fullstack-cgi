d4063c28823eec534c504af6809fb0169b54e428
thulasiram <thulasiram4105@gmail.com>
2023-05-15 13:16:56 +0530

    first commit

---
+++ src/main/java/org/orders/management/model/Orders.java
@@ -0,0 +1,48 @@
+package org.orders.management.model;
+
+public class Orders {
+	private int id;
+	private String orderName;
+	private int orderPrice;
+
+	public Orders() {
+		super();
+	}
+
+	public Orders(int id, String orderName, int orderPrice) {
+		super();
+		this.id = id;
+		this.orderName = orderName;
+		this.orderPrice = orderPrice;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public int getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(int orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Orders [id=" + id + ", orderName=" + orderName + ", orderPrice=" + orderPrice + "]";
+	}
+
+}
